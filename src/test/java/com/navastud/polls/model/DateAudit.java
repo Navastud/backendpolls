b193dc6b5df1be0a4e6c05abd7b20b49fcd7b106
navastud <david.navastud@gmail.com>
2019-07-09 23:46:55 -0400

    1: Creation of domain models
    Task-Url: https://github.com/Navastud/polls/issues/1

---
+++ src/test/java/com/navastud/polls/model/DateAudit.java
@@ -0,0 +1,64 @@
+package com.navastud.polls.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt" }, allowGetters = true)
+public class DateAudit implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6902012339386366483L;
+
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
+
+	public DateAudit() {
+		super();
+	}
+
+	public DateAudit(LocalDateTime createdAt, LocalDateTime updatedAt) {
+		super();
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
