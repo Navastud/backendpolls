d444a4538b3155e7a84707984088396c4b76e067
navastud <david.navastud@gmail.com>
2019-07-10 02:59:44 -0400

    10: Writing the Login and Signup APIs
    Task-Url: https://github.com/Navastud/polls/issues/10

---
+++ src/main/java/com/navastud/polls/payload/ApiResponse.java
@@ -0,0 +1,30 @@
+package com.navastud.polls.payload;
+
+public class ApiResponse {
+
+	private Boolean success;
+	private String message;
+
+	public ApiResponse(Boolean success, String message) {
+		super();
+		this.success = success;
+		this.message = message;
+	}
+
+	public Boolean getSuccess() {
+		return success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
