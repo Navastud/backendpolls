3b950be25202a66e018b6bbeb4ee79e3d5f1181c
navastud <navastud@soulteker>
2019-07-11 11:53:39 -0400

    Squashed commit of the following:
    commit 8d96680a674582a1d312675e8a17ea39a377abb3
    Author: navastud <navastud@soulteker>
    Date:   Thu Jul 11 11:51:02 2019 -0400
    
    	26: Repositories

---
+++ src/main/java/com/navastud/polls/model/ChoiceVoteCount.java
@@ -0,0 +1,31 @@
+package com.navastud.polls.model;
+
+public class ChoiceVoteCount {
+
+	private Long choiceId;
+
+	private Long voteCount;
+
+	public ChoiceVoteCount(Long choiceId, Long voteCount) {
+		super();
+		this.choiceId = choiceId;
+		this.voteCount = voteCount;
+	}
+
+	public Long getChoiceId() {
+		return choiceId;
+	}
+
+	public void setChoiceId(Long choiceId) {
+		this.choiceId = choiceId;
+	}
+
+	public Long getVoteCount() {
+		return voteCount;
+	}
+
+	public void setVoteCount(Long voteCount) {
+		this.voteCount = voteCount;
+	}
+
+}
